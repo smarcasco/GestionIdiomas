aab74eb40c241cbceb6288d1a51d79d02f0bad6e
SERGIOPC\sergio <sergito91@gmail.com>
2017-07-02 17:13:14 +0200

    Initial commit

---
+++ app/src/main/java/org/sergioapps/gestionidiomas/bean/PalabraBean.java
@@ -0,0 +1,8 @@
+package org.sergioapps.gestionidiomas.bean;
+
+/**
+ * Created by sergio on 02/07/2017.
+ */
+
+public class PalabraBean {
+}
---
+++ app/src/main/java/org/sergioapps/gestionidiomas/dao/PalabraDAO.java
@@ -0,0 +1,8 @@
+package org.sergioapps.gestionidiomas.dao;
+
+/**
+ * Created by sergio on 02/07/2017.
+ */
+
+public class PalabraDAO {
+}
